5979dde44d90178eca516ad680ab121c5878ebaa
Longbiao CHEN <longbiaochen@icloud.com>
2019-04-11 15:41:31 +0800

    Quick commit at 2019-04-11-15:41:31

--- design-patterns/src/abstract_factory/HaierFactory.java
+++ design-patterns/src/abstract_factory/HaierFactory.java
@@ -4,11 +4,13 @@ public class HaierFactory implements EFactory
 {
 	public Television produceTelevision()
 	{
+		System.out.println("正在生产海尔电视...");
 		return new HaierTelevision();
 	}
 	
 	public AirConditioner produceAirConditioner()
 	{
+		System.out.println("正在生产海尔空调...");
 		return new HairAirConditioner();
 	}
 }
--- design-patterns/src/abstract_factory/TCLFactory.java
+++ design-patterns/src/abstract_factory/TCLFactory.java
@@ -4,11 +4,13 @@ public class TCLFactory implements EFactory
 {
 	public Television produceTelevision()
 	{
+		System.out.println("正在生产TCL电视...");
 		return new TCLTelevision();
 	}
 	
 	public AirConditioner produceAirConditioner()
 	{
+		System.out.println("正在生产TCL空调...");
 		return new TCLAirConditioner();
 	}
 }
