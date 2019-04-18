e7660a1d30844c580a1333c2d1126624043c6e0f
Longbiao CHEN <longbiaochen@icloud.com>
2019-04-18 15:04:50 +0800

    Quick commit at 2019-04-18-15:04:50

---
+++ design-patterns/src/decorator/Changer.java
@@ -0,0 +1,13 @@
+package decorator;
+
+public class Changer implements Transform {
+    private Transform transform;
+
+    public Changer(Transform transform) {
+	this.transform = transform;
+    }
+
+    public void move() {
+	transform.move();
+    }
+}
---
+++ design-patterns/src/decorator/Client.java
@@ -0,0 +1,14 @@
+package decorator;
+
+public class Client {
+    public static void main(String args[]) {
+	Transform camaro;
+	camaro = new Car();
+	camaro.move();
+	System.out.println("-----------------------------");
+
+	Airplane bumblebee = new Airplane(camaro);
+	bumblebee.move();
+	bumblebee.fly();
+    }
+}
